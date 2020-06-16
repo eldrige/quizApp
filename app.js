ebd5910bf3cc985c760572aa11a07276d6d9bd31
Apoh Eldrige <peldrige8@gmail.com>
2020-06-17 00:07:55 +0100

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use-strict";
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 
 const questionContainerElt = document.getElementById("question-container");
 
@@ -13,6 +14,10 @@ let shuffledQuestions, currentQuestionIndex;
 // undeclaring without assigning them values, leave them undefined
 
 startButton.addEventListener("click", start);
+nextButton.addEventListener("click", ()=>{
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function start() {
   startButton.classList.add("hide"); // on button click hide it
@@ -24,6 +29,7 @@ function start() {
 }
 
 function setNextQuestion() {
+    resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex]); //take first question in the array, since currQuestionIndex is set to 0
 }
 
@@ -43,9 +49,43 @@ function showQuestion(question) {
     answerBtnsElt.appendChild(button);
   });
 }
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerBtnsElt.firstChild){ //if there is a child inside answerbuttonElement , that is for all elts found in the answerBtnsElt
+        answerBtnsElt.removeChild(answerBtnsElt.firstChild) //remove the children
+    }
+}
 
-function selectAnswer() {
+function selectAnswer(e) {
+    const selectedBtn = e.target //whatever btn we click on
+    const correct = selectedBtn.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerBtnsElt.children).forEach(button => { //the answerBtnsElt is a liveList, it need to be converted to an array, before using the foreach method
+        setStatusClass(button,button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1){ //if we stil have questions
+    nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classLilst.remove('hide')
+    } //else if questions are finished you remove the next button, adn display the starrt button
     
+}
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct') //if correct add thr correct class
+    } else {
+        element.classList.add('wrong')
+    }
+
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 const questions = [
     {
@@ -55,29 +95,29 @@ const questions = [
         { text: "22", correct: false },
       ],
     },
-    // {
-    //   question: "Who is the best YouTuber?",
-    //   answers: [
-    //     { text: "Web Dev Simplified", correct: true },
-    //     { text: "Traversy Media", correct: true },
-    //     { text: "Dev Ed", correct: true },
-    //     { text: "Fun Fun Function", correct: true },
-    //   ],
-    // },
-    // {
-    //   question: "Is web development fun?",
-    //   answers: [
-    //     { text: "Kinda", correct: false },
-    //     { text: "YES!!!", correct: true },
-    //     { text: "Um no", correct: false },
-    //     { text: "IDK", correct: false },
-    //   ],
-    // },
-    // {
-    //   question: "What is 4 * 2?",
-    //   answers: [
-    //     { text: "6", correct: false },
-    //     { text: "8", correct: true },
-    //   ],
-    // },
+    {
+      question: "Who is the best YouTuber?",
+      answers: [
+        { text: "Web Dev Simplified", correct: true },
+        { text: "Traversy Media", correct: true },
+        { text: "Dev Ed", correct: true },
+        { text: "Fun Fun Function", correct: true },
+      ],
+    },
+    {
+      question: "Is web development fun?",
+      answers: [
+        { text: "Kinda", correct: false },
+        { text: "YES!!!", correct: true },
+        { text: "Um no", correct: false },
+        { text: "IDK", correct: false },
+      ],
+    },
+    {
+      question: "What is 4 * 2?",
+      answers: [
+        { text: "6", correct: false },
+        { text: "8", correct: true },
+      ],
+    },
   ];
